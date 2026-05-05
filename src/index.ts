456935b14d9fb6409f7985a7043fc9d0a65826b3
nshiab <nael.shiab@gmail.com>
2026-05-05 15:40:53 -0400

    v1.2.21

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+/**
+ * @module
+ *
+ * The Journalism library (AI functions)
+ *
+ * To install the library with Deno, use:
+ * ```bash
+ * deno add jsr:@nshiab/journalism-ai
+ * ```
+ *
+ * To install the library with Node.js, use:
+ * ```bash
+ * npm i @nshiab/journalism-ai
+ * ```
+ *
+ * To import a function, use:
+ * ```ts
+ * import { functionName } from "@nshiab/journalism-ai";
+ * ```
+ */
+
 import askAI from "./ai/askAI.ts";
 import askAIPool from "./ai/askAIPool.ts";
 import getEmbedding from "./ai/getEmbedding.ts";
